fc82d96d757e835480dac11a425234d332067c33
Hamidul-1 <hamidul786cse@gmail.com>
2022-02-15 21:05:48 +0600

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
+const detailsButton = document.getElementById('detail-submit-btn');
 
+
+detailsButton.addEventListener('click',function(){
+
+  const buyerDetails = document.getElementById('buyer-details-input');
+  // console.log('buyer-details',buyerDetails);
+  document.getElementById('buyer-info').innerText = buyerDetails.value;
+  buyerDetails.value = '';
+  
+})
+
+const addButton = document.getElementById('add-details-btn').addEventListener('click',function(){
+  const infoTable = document.getElementById('info-table');
+  const itemName = document.getElementById('item-name-input');
+  const itemPrice = document.getElementById('item-price-input');
+  const itemQuantity = document.getElementById('item-quantity-input');
+  console.log(itemName.value, itemPrice.value, itemQuantity.value);
+  
+  {/* <tr>
+    <th >1</th>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+  </tr> */}
+  const totalPrice = parseFloat(itemPrice.value) * parseFloat(itemQuantity.value);
+  const tr = document.createElement('tr');
+  const th = document.createElement('th');
+  const td1 = document.createElement('td');
+  const td2 = document.createElement('td');
+  const td3 = document.createElement('td');
+
+  th.innerText = itemName.value;
+  td1.innerText = itemPrice.value;
+  td2.innerText = itemQuantity.value;
+  td3.innerText = totalPrice;
+
+  tr.appendChild(th);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  infoTable.appendChild(tr);
+})
 
 
 
